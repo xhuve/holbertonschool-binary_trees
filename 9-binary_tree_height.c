0fafbb5083bf623a7bd8342ebaeaa679b4967fae
xhuve <xhuvelialbana@gmail.com>
2024-04-10 22:08:23 +0200

    Finish 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,13 +9,13 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t right = 0, left = 0;
+	size_t left = 0, right = 0;
 
-	if (tree == NULL)
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
 
-	right = binary_tree_height(tree->right);
 	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
 
 	if (left > right)
 		return (left + 1);
