0ca0352d9108a08fddcd94cd0201ed5c2a662ea1
xhuve <xhuvelialbana@gmail.com>
2024-04-10 11:56:25 +0200

    Finish 7

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+* binary_tree_inorder - check code
+*
+* @tree: pointer
+* @func: function pointer
+* Return: new node
+*/
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+
+	if (tree == NULL || func == NULL)
+		return;
+
+	if (tree->left != NULL)
+	{
+		binary_tree_inorder(tree->left, func);
+	}
+
+	func(tree->n);
+
+	if (tree->right != NULL)
+	{
+		binary_tree_inorder(tree->right, func);
+	}
+
+}
