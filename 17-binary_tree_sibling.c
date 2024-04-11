0a32dbd01f233cde897c0780db90f320d1b9f5e4
xhuve <xhuvelialbana@gmail.com>
2024-04-11 13:14:57 +0200

    Finish 17

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+* binary_tree_is_perfect - check code
+*
+* @tree: pointer
+* Return: new node
+*/
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	binary_tree_t *parent;
+
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	parent = node->parent;
+
+	if (parent->left == node)
+		return (parent->right);
+	else if (parent->right == node)
+		return (parent->left);
+	else
+		return (NULL);
+}
